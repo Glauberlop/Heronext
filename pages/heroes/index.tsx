17742a87b988f237b4fe3b5d602c9a16123f5b9d
Glauberlop <glauberlop@gmail.com>
2022-06-09 12:36:27 -0300

    initial

---
+++ pages/heroes/index.tsx
@@ -0,0 +1,44 @@
+import type { NextPage } from 'next';
+import { GetStaticProps } from 'next';
+import {Hero} from './types';
+
+
+export interface Heroes {
+    heroes: Hero[];
+}
+
+
+
+const mapFn = (hero: Hero ) => {
+    return <div key={hero.id}>
+    <h3>{hero.name}</h3>
+    <img src={hero.images.md} alt={hero.name}></img>
+
+    </div>
+  
+    }
+  
+const Heroes: NextPage <{response: Heroes }> = (props) => {
+    console.log(props)
+    
+    const {response} = props
+    const {heroes} = response;
+           
+    return <>
+        {heroes.map(mapFn)}
+     </>
+}
+
+export const getStaticProps: GetStaticProps<{response: Heroes }> = async () =>{
+    const data = await fetch('https://akabab.github.io/superhero-api/api/all.json')
+    const response:Hero[] = await data.json()  
+    
+    
+    
+   return {
+       props: { response: {heroes: response} }
+   }   
+  
+}
+
+export default Heroes
